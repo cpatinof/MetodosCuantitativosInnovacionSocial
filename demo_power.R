432602fb4d3e24701a2956b327c34159d3733b97
cpatinof <cpatinof@gmail.com>
2015-08-05 18:01:30 -0500

    demo sobre poder en pruebas de hipótesis

---
+++ demo_power.R
@@ -0,0 +1,44 @@
+## Demo: Power for Hypothesis Testing
+## Carlos Patiño
+## Aug 3, 2015
+
+# Librerias
+library(manipulate)
+library(ggplot2)
+
+# Parámetros
+mu0 <- 30
+
+# Funciones
+myplot <- function(sigma,mua,n,alpha) {
+        g <- ggplot(data.frame(mu=c(27,36)), aes(x=mu))
+        g <- g + stat_function(fun=dnorm, geom="line",
+                               args=list(mean=mu0, sd=sigma/sqrt(n)),
+                               size=2, col="red")
+        g <- g + stat_function(fun=dnorm, geom="line",
+                               args=list(mean=mua, sd=sigma/sqrt(n)),
+                               size=2, col="blue")
+        xitc <- mu0 + qnorm(1-alpha)*sigma/sqrt(n)
+        g <- g + geom_vline(xintercept=xitc, size=3)
+        g
+}
+
+# Demo
+manipulate(
+        myplot(sigma,mua,n,alpha),
+        sigma=slider(1,10,step=1,initial=4),
+        mua=slider(30,35,step=1,initial=32),
+        n=slider(1,50,step=1,initial=16),
+        alpha=slider(0.01,0.1,step=0.01,initial=0.05)
+        )
+
+# Power
+power.t.test(n=16, delta=2, sd=4, type="one.sample", alt="one.sided")$power
+power.t.test(power=0.8, delta=2, sd=4, type="one.sample", alt="one.sided")$n
+
+# Extra
+p <- ggplot(data.frame(mu=c(-4,4)), aes(x=mu))
+p <- p + stat_function(fun=dnorm,geom="line",args=list(mean=0,sd=1),size=2,col="red")
+p <- p + geom_vline(xintercept=1.64,size=3)
+p <- p + stat_function(fun=dnorm,geom="line",args=list(mean=2,sd=1),size=2,col="darkgreen")
+p
